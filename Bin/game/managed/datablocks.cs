3aa664ed561a3f5ac71036cd3589a7819e6c9520
elsonZ <769255844@qq.com>
2017-05-19 12:39:14 +0800

    add BombMan AnimateSprite

--- Bin/game/managed/datablocks.cs
+++ Bin/game/managed/datablocks.cs
@@ -2,4 +2,124 @@ $managedDatablockSet = new SimSet() {
    canSaveDynamicFields = "1";
       setType = "Datablocks";
 
+   new t2dImageMapDatablock(PlayerPic03ImageMap) {
+      imageName = "~/data/images/PlayerPic03.dds";
+      imageMode = "CELL";
+      frameCount = "-1";
+      filterMode = "SMOOTH";
+      filterPad = "0";
+      preferPerf = "0";
+      cellRowOrder = "1";
+      cellOffsetX = "0";
+      cellOffsetY = "0";
+      cellStrideX = "0";
+      cellStrideY = "0";
+      cellCountX = "-1";
+      cellCountY = "-1";
+      cellWidth = "42";
+      cellHeight = "93";
+      preload = "0";
+      allowUnload = "0";
+      force16Bit = "0";
+   };
+   new t2dAnimationDatablock(PlayerPic03Animation1) {
+      imageMap = "PlayerPic03ImageMap";
+      animationFrames = "0 1 2 3";
+      animationTime = "0.666656";
+      animationCycle = "1";
+      randomStart = "0";
+      startFrame = "0";
+      animationPingPong = "0";
+      animationReverse = "0";
+   };
+   new t2dAnimationDatablock(PlayerPic03Animation2) {
+      imageMap = "PlayerPic03ImageMap";
+      animationFrames = "6 7 8 9";
+      animationTime = "0.666656";
+      animationCycle = "1";
+      randomStart = "0";
+      startFrame = "0";
+      animationPingPong = "0";
+      animationReverse = "0";
+   };
+   new t2dAnimationDatablock(PlayerPic03Animation3) {
+      imageMap = "PlayerPic03ImageMap";
+      animationFrames = "12 13 14 15";
+      animationTime = "0.666667";
+      animationCycle = "1";
+      randomStart = "0";
+      startFrame = "0";
+      animationPingPong = "0";
+      animationReverse = "0";
+   };
+   new t2dAnimationDatablock(PlayerPic03Animation4) {
+      imageMap = "PlayerPic03ImageMap";
+      animationFrames = "18 19 20 21";
+      animationTime = "0.666667";
+      animationCycle = "1";
+      randomStart = "0";
+      startFrame = "0";
+      animationPingPong = "0";
+      animationReverse = "0";
+   };
+   new t2dImageMapDatablock(PlayerPic04ImageMap) {
+      imageName = "~/data/images/PlayerPic04.dds";
+      imageMode = "CELL";
+      frameCount = "-1";
+      filterMode = "SMOOTH";
+      filterPad = "0";
+      preferPerf = "0";
+      cellRowOrder = "1";
+      cellOffsetX = "0";
+      cellOffsetY = "0";
+      cellStrideX = "0";
+      cellStrideY = "0";
+      cellCountX = "-1";
+      cellCountY = "-1";
+      cellWidth = "44";
+      cellHeight = "93";
+      preload = "0";
+      allowUnload = "0";
+      force16Bit = "0";
+   };
+   new t2dAnimationDatablock(PlayerPic04Animation1) {
+      imageMap = "PlayerPic04ImageMap";
+      animationFrames = "0 1 2 3";
+      animationTime = "0.666667";
+      animationCycle = "1";
+      randomStart = "0";
+      startFrame = "0";
+      animationPingPong = "0";
+      animationReverse = "0";
+   };
+   new t2dAnimationDatablock(PlayerPic04Animation2) {
+      imageMap = "PlayerPic04ImageMap";
+      animationFrames = "5 6 7 8";
+      animationTime = "0.666667";
+      animationCycle = "1";
+      randomStart = "0";
+      startFrame = "0";
+      animationPingPong = "0";
+      animationReverse = "0";
+   };
+   new t2dAnimationDatablock(PlayerPic04Animation3) {
+      imageMap = "PlayerPic04ImageMap";
+      animationFrames = "10 11 12 13";
+      animationTime = "0.666667";
+      animationCycle = "1";
+      randomStart = "0";
+      startFrame = "0";
+      animationPingPong = "0";
+      animationReverse = "0";
+   };
+   new t2dAnimationDatablock(PlayerPic04Animation4) {
+      imageMap = "PlayerPic04ImageMap";
+      animationFrames = "15 16 17 18";
+      animationTime = "0.666667";
+      animationCycle = "1";
+      randomStart = "0";
+      startFrame = "0";
+      animationPingPong = "0";
+      animationReverse = "0";
+   };
 };
